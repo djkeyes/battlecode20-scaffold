fc57d8bbe79cdf4fe37f22c0c4ba9f2f5beb402f
Daniel Keyes <danieljkeyes@gmail.com>
2020-01-11 16:46:07 -0600

    copy landscaperwaller to pathfinding as a working copy

---
+++ src/pathfinding/MapSymmetry.java
@@ -0,0 +1,49 @@
+package pathfinding;
+
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+/**
+ * Provides utility functions to reason about map symmetry.
+ */
+public final class MapSymmetry {
+
+    public static final int VERTICAL_SYMMETRY = 0;
+    public static final int HORIZONTAL_SYMMETRY = 1;
+    public static final int ROTATIONAL_SYMMETRY = 2;
+
+    public static final boolean[] isSymmetryPossible = {true, true, true};
+
+    public static MapLocation[] getAllSymmetricCoords(final RobotController rc, final MapLocation location) {
+        return new MapLocation[]{
+                getVerticalSymmetricCoords(rc, location),
+                getHorizontalSymmetricCoords(rc, location),
+                getRotationalSymmetricCoords(rc, location)
+        };
+    }
+
+    public static MapLocation getSymmetricCoords(final RobotController rc, final MapLocation location,
+                                                 final int symmetry) {
+        switch (symmetry) {
+            case VERTICAL_SYMMETRY:
+                return getVerticalSymmetricCoords(rc, location);
+            case HORIZONTAL_SYMMETRY:
+                return getHorizontalSymmetricCoords(rc, location);
+            case ROTATIONAL_SYMMETRY:
+                return getRotationalSymmetricCoords(rc, location);
+        }
+        return getVerticalSymmetricCoords(rc, location);
+    }
+
+    public static MapLocation getVerticalSymmetricCoords(final RobotController rc, final MapLocation location) {
+        return new MapLocation(rc.getMapWidth() - location.x - 1, location.y);
+    }
+
+    public static MapLocation getHorizontalSymmetricCoords(final RobotController rc, final MapLocation location) {
+        return new MapLocation(location.x, rc.getMapHeight() - location.y - 1);
+    }
+
+    public static MapLocation getRotationalSymmetricCoords(final RobotController rc, final MapLocation location) {
+        return new MapLocation(rc.getMapWidth() - location.x - 1, rc.getMapHeight() - location.y - 1);
+    }
+}
